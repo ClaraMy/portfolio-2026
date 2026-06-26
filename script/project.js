12e48bb0c4a62180a3ba21035c0ab312653fdf31
clara many <clara.many@edu.univ-eiffel.fr>
2026-06-26 15:30:40 +0200

    edit header title

--- script/project.js
+++ script/project.js
@@ -9,6 +9,9 @@ const projectInfoText = document.querySelector(".project-info-text");
 const projectLinks = document.querySelector(".project-links");
 
 function showProject(project) {
+
+  document.title = `${project.title} - Many Clara Portfolio`;
+
   // show hero
   projectTitle.textContent = project.title;
   projectShortDescription.textContent = project.shortDescription;
