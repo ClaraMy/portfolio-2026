cb8b1025a9ce877534dba5f4be750c12a35d2772
clara many <clara.many@edu.univ-eiffel.fr>
2026-06-26 16:56:43 +0200

    remove english / french script

--- script/script.js
+++ script/script.js
@@ -1,26 +1,3 @@
-let lang = "french"; // Default language
-
-function updateTexts(lang) {
-  fetch(`${lang}.json`).then(function (response) {
-    response.json().then(function (data) {
-      Object.keys(data).forEach(function (key) {
-        document.querySelector("#" + key).innerHTML = data[key];
-      });
-    });
-  });
-}
-
-updateTexts(lang);
-
-const buttons = document.querySelectorAll(".lang-btn");
-
-buttons.forEach(function (button) {
-  button.addEventListener("click", () => {
-    const selectedLang = button.getAttribute("data-language");
-    updateTexts(selectedLang);
-  });
-});
-
 // Load Projects
 async function loadProjects() {
   const response = await fetch("./json/projects.json");
